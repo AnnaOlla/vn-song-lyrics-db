5236c738746e9e2bdc9647acc2de1a907281ddf0
AnnaOlla <140060248+AnnaOlla@users.noreply.github.com>
2026-03-07 11:01:50 +0400

    Including new files into autoload

--- core/boot.php
+++ core/boot.php
@@ -4,6 +4,7 @@ require_once 'core/router.php';
 
 require_once 'include/enums.php';
 require_once 'include/juliamo-captcha.php';
+require_once 'include/parsing-functions.php';
 require_once 'include/session-functions.php';
 require_once 'include/validation-functions.php';
 
