e3ab962486fcfd763ab8a63b2b26704828f1f8c5
AnnaOlla <140060248+AnnaOlla@users.noreply.github.com>
2026-03-07 10:18:36 +0400

    Added the captcha class to autoload

--- core/boot.php
+++ core/boot.php
@@ -3,6 +3,7 @@
 require_once 'core/router.php';
 
 require_once 'include/enums.php';
+require_once 'include/juliamo-captcha.php';
 require_once 'include/session-functions.php';
 require_once 'include/validation-functions.php';
 
