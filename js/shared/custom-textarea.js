2f9b8c88b97400f803a03bff2c61eac0ed5e6ddf
AnnaOlla <140060248+AnnaOlla@users.noreply.github.com>
2026-04-15 18:54:26 +0400

    Delete js/shared/custom-textarea.js

--- js/shared/custom-textarea.js
+++
@@ -1,9 +0,0 @@
-function autoresizeTextarea(e) {
-	const textarea = e.target;
-	const text     = textarea.value;
-	textarea.rows  = (text.match(/\n/g) || []).length + 1;
-}
-
-function addEventListenersToCustomTextarea(textarea) {
-	textarea.addEventListener('input', autoresizeTextarea);
-}
