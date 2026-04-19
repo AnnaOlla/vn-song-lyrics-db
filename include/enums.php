7fff9cfa2c42abe56f6de42d9787eb6b5e380128
AnnaOlla <140060248+AnnaOlla@users.noreply.github.com>
2026-04-19 17:52:46 +0400

    Deleted unused enum values

--- include/enums.php
+++ include/enums.php
@@ -16,43 +16,25 @@ enum StaticAsset: string
 	case NoAlbum     = 'no-album';
 	case NoArtist    = 'no-artist';
 	case NoCharacter = 'no-character';
-	
-	/*
-	case BadRequest400          = 'bad-request-400';
-	case Unauthorized401        = 'unauthorized-401';
-	case PaymentRequired402     = 'payment-required-402';
-	case Forbidden403           = 'forbidden-403';
-	case NotFound404            = 'not-found-404';
-	case InternalServerError500 = 'internal-server-error-500';
-	*/
 }
 
-enum AuthenticationError
+enum InputError
 {
 	case None;
 	
 	case CaptchaInvalid;
 	
-	case EmptyEmail;
-	case EmptyPassword;
 	case EmailNotFound;
-	case EmailNotExists;
-	case IncorrectPassword;
+	case EmailInvalid;
+	case EmailTaken;
 	
-	case UsernameTrimmable;
 	case UsernameForbiddenSymbols;
 	case UsernameLengthIncorrect;
 	case UsernameTaken;
 	
-	case EmailTaken;
-	case EmailInvalid;
-	
-	case PasswordTrimmable;
+	case IncorrectPassword;
 	case PasswordForbiddenSymbols;
 	case PasswordLengthIncorrect;
-	
-	case AccountNotVerified;
-	case MailSendFailed;
 }
 
 enum EntityStatus
